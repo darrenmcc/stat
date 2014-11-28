3ae814f20f053e7ec5462d9a0b5cb1a320c64e08
Jonathan J Lawlor <jonathan.lawlor@gmail.com>
2014-11-27 19:02:24 -0500

    implement & test Mean type

--- online/mean.go
+++ online/mean.go
@@ -4,47 +4,83 @@
 
 package incremental
 
+// Adapted from Pébay, Philippe. "Formulas for robust, one-pass parallel
+// computation of covariances and arbitrary-order statistical moments."
+// Sandia Report SAND2008-6212, Sandia National Laboratories (2008)
+
 type mean struct {
-	Sum float64
-	N   int
+	M float64
+	N float64
 }
 
 // NewMean creates a new incremental mean measurement.
-func NewMean() *mean {
-	return new(mean)
+func NewMean(x ...float64) *mean {
+	m := new(mean)
+	m.IncrementSlice(x)
+	return m
 }
 
 // Increment adds a new sample to take a mean over.
-func (m *mean) Increment(v float64) {
-	m.Sum += v
+func (m *mean) Increment(y float64) {
 	m.N++
+	m.M += (y - m.M) / m.N
+}
+
+// IncrementWeighted adds a new sample to take a mean over, using a given weight.
+func (m *mean) IncrementWeighted(y, weight float64) {
+	if weight == 0.0 {
+		return
+	}
+	m.N += weight
+	m.M += (y - m.M) * weight / m.N
 }
 
 // Result returns the current mean Result.
 func (m *mean) Result() float64 {
-	return m.Sum / float64(m.N)
+	return m.M
 }
 
-// should this have a combine, which adds two means together?
-// I think it should, because it can give the building blocks for how
-// to perform it concurrently
-/*
-func (m1 *mean) Combine(m2 *mean) *mean {
+// Combine two Means into a single Mean.
+func Combine(m1, m2 *mean) *mean {
+	if m1.N == 0 && m2.N == 0 {
+		return NewMean()
+	}
 	return &mean{
-		Sum: m1.Sum + m2.Sum
-		N: m1.N + m2.N
+		M: m1.M + m2.N/(m1.N+m2.N)*(m2.M-m1.M),
+		N: m1.N + m2.N,
 	}
 }
-*/
 
-// should this have an IncrementMany, which ranges over input slices?
-// I think it shouldn't, because it is partially redundant to Increment, but
-// it does avoid repeatedly incrementing the count N.
-/*
-func (m *mean) IncrementMany(x ...float64) *mean {
-	for _, v := range x {
-		m.Sum += v
+// IncrementSlice updates a Mean with a slice of inputs.
+func (m *mean) IncrementSlice(x []float64) {
+	var mu, N float64
+	N = float64(len(x))
+	if N == 0 {
+		return
 	}
-	m.N += len(x)
+	for _, y := range x {
+		mu += y / N
+	}
+	m.N += N
+	m.M += N / m.N * (mu - m.M)
 }
-*/
+
+// IncrementSliceWeighted updates a Mean with a weighted slice of inputs.
+func (m *mean) IncrementSliceWeighted(x, weights []float64) {
+	if len(x) != len(weights) {
+		panic("stat: slice length mismatch")
+	}
+
+	// this uses a pairwise approach, which should increase numerical stability.
+	var mu, N float64
+	for i, weight := range weights {
+		if weight == 0.0 {
+			continue
+		}
+		N += weight
+		mu += (x[i] - mu) * weight / N
+	}
+	m.N += N
+	m.M += N / m.N * (mu - m.M)
+
+}
--- online/mean_test.go
+++ online/mean_test.go
@@ -9,7 +9,18 @@ import (
 	"testing"
 )
 
-func TestMean(t *testing.T) {
+func Panics(fun func()) (b bool) {
+	defer func() {
+		err := recover()
+		if err != nil {
+			b = true
+		}
+	}()
+	fun()
+	return
+}
+
+func TestMeanIncrement(t *testing.T) {
 	for i, test := range []struct {
 		x   []float64
 		ans []float64
@@ -19,12 +30,78 @@ func TestMean(t *testing.T) {
 			ans: []float64{8, 2.5, 4, 5, 6},
 		},
 	} {
-		m := NewMean()
+		m1 := NewMean()
 		for j, v := range test.x {
-			m.Increment(v)
-			if mu := m.Result(); math.Abs(test.ans[j]-mu) > 1e-14 {
-				t.Errorf("Incremental mean mismatch case %d. Expected %v, Found %v", i, test.ans[j], mu)
+			m1.Increment(v)
+			if mu := m1.Result(); math.Abs(test.ans[j]-mu) > 1e-14 {
+				t.Errorf("Increment mean mismatch case %d, %d. Expected %v, Found %v", i, j, test.ans[j], mu)
+			}
+			m2 := NewMean()
+			m2.M = m1.M
+			m2.N = m1.N
+
+			m2.IncrementSlice(test.x[j+1:])
+			if mu := m2.Result(); math.Abs(test.ans[len(test.ans)-1]-mu) > 1e-14 {
+				t.Errorf("IncrementalSlice mean mismatch case %d, %d. Expected %v, Found %v", i, j, test.ans[len(test.ans)-1], mu)
 			}
 		}
 	}
+}
+
+func TestMeanIncrementWeighted(t *testing.T) {
+	for i, test := range []struct {
+		x   []float64
+		wts []float64
+		ans []float64
+	}{
+		{
+			x:   []float64{8, -3, 7, 8, 10},
+			wts: []float64{1, 0, 1, -1, 1},
+			ans: []float64{8, 8, 7.5, 7, 8.5},
+		},
+	} {
+		m := NewMean()
+		for j, v := range test.x {
+			m.IncrementWeighted(v, test.wts[j])
+			if mu := m.Result(); math.Abs(test.ans[j]-mu) > 1e-14 {
+				t.Errorf("IncrementWeighted mean mismatch case %d, %d. Expected %v, Found %v", i, j, test.ans[j], mu)
+			}
+
+			m2 := NewMean()
+			m2.M = m.M
+			m2.N = m.N
+
+			m2.IncrementSliceWeighted(test.x[j+1:], test.wts[j+1:])
+			if mu := m2.Result(); math.Abs(test.ans[len(test.ans)-1]-mu) > 1e-14 {
+				t.Errorf("IncrementalSliceWeighted mean mismatch case %d, %d. Expected %v, Found %v", i, j, test.ans[len(test.ans)-1], mu)
+			}
+		}
+	}
+	if !Panics( func() { NewMean().IncrementSliceWeighted(make([]float64, 2), make([]float64, 3)) } ) {
+		t.Errorf("IncrementSliceWeighted did not panic with x, length length mismatch")
+	}
+	
+}
+
+func TestMeanCombine(t *testing.T) {
+	for i, test := range []struct {
+		x1, x2 []float64
+		ans    float64
+	}{
+		{
+			x1:  []float64{4, -3, 7, 8, 10},
+			x2:  []float64{1, 2, 3, 4},
+			ans: 4, }, 
+		{		
+			ans: 0,
+		},
+	} {
+		m1 := NewMean(test.x1...)
+		m2 := NewMean(test.x2...)
+		m3 := Combine(m1, m2)
+		if mu := m3.Result(); math.Abs(test.ans-mu) > 1e-14 {
+			t.Errorf("Combined mean mismatch case %d. Expected %v, Found %v", i, test.ans, mu)
+		}
+	}
+	
 }
